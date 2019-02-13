0e0720de6f06d1a7b39922fb5fe47b602a9181f2
Magdalena Semik <mgd.kaczmarek@gmail.com>
2019-02-13 22:40:36 +0100

    make basics of header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Time from './Time';
+
+const Header = () => (
+    <header className="header">
+        <Time />
+        <h1 className="title">CALORIES COUNTER</h1>
+    </header>
+);
+
+export default Header;
